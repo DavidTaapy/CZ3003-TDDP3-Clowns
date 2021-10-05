b5873e91ce6df297afa77025069219996e08f5ea
ryantanyx <ryantanyx@gmail.com>
2021-10-05 23:17:38 +0800

    user routes, controller, model

--- Nodejs_Server/app.js
+++ Nodejs_Server/app.js
@@ -13,6 +13,9 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
+
+require("./app/routes/user.routes.js")(app);
+
 // set port, listen for requests
 app.listen(3000, () => {
   console.log("Server is running on port 3000.");
---
+++ Nodejs_Server/controllers/user.controller.js
@@ -0,0 +1,27 @@
+const User = require("../models/user.model.js");
+
+// Create and Save a new Customer
+exports.create = (req, res) => {
+  
+};
+
+// Retrieve all Customers from the database.
+/* Leave blank first
+exports.findAll = (req, res) => {
+  
+};*/
+
+// Find a single Customer with a customerId
+exports.findOne = (req, res) => {
+  
+};
+
+// Update a Customer identified by the customerId in the request
+exports.update = (req, res) => {
+  
+};
+
+// Delete a Customer with the specified customerId in the request
+exports.delete = (req, res) => {
+
+};
