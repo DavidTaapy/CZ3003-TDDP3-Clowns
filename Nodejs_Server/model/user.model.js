370e5b11b7357e74099006db683ab4259e067c8c
joy00000 <joy00000@users.noreply.github.com>
2021-10-13 12:37:29 +0800

    edited models

--- Nodejs_Server/model/user.model.js
+++ Nodejs_Server/model/user.model.js
@@ -1,11 +1,12 @@
 class User {
-    constructor(userName, primaryLevel){
+    constructor(id, userName, primaryLevel){
+        this.id = id;
         this.userName = userName;
         this.primaryLevel = primaryLevel;
         this.eloRating = 0;
         this.points = 0;
-        //this.coins = 0;
         this.inventory = [];
+        this.questions = [];
     }
 }
 
