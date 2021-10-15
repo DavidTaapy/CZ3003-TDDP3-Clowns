823f48f30233c1d8013291c842cc26f5ea4a004d
jennifer <gwyjennifer@gmail.com>
2021-10-15 11:58:40 +0800

    Modified game end screen text output

--- Unity_Client/Assets/Scripts/EndScreen.cs
+++ Unity_Client/Assets/Scripts/EndScreen.cs
@@ -16,6 +16,8 @@ public class EndScreen : MonoBehaviour
     public void ShowFinalScore()
     {
         finalScoreText.text = "Congratulations!\nYou got a score of " + 
-                                scoreKeeper.CalculateScore() + "%";
+                                scoreKeeper.CalculateScore() + "%!\n You have answered "
+                                + scoreKeeper.GetCorrectAnswers() + " out of " +
+                                scoreKeeper.GetQuestionSeen() + " correctly!";
     }
 }
