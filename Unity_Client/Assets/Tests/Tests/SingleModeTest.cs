7ff268c8ca15e2e19caf8f07b837c859d958206b
jennifer <gwyjennifer@gmail.com>
2021-10-16 18:20:03 +0800

    Update question and add SingleMode test

---
+++ Unity_Client/Assets/Tests/Tests/SingleModeTest.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SingleModeTest
+{
+    [UnityTest]
+    public IEnumerator SkipQuestionIncreaseCorrectQuestion()
+    {
+        SceneManager.LoadScene("SingleMode");
+        // Use yield to skip a frame.
+        yield return new WaitForSeconds(3);
+
+        Button skipQuestionButton = GameObject.Find("SkipQuestionButton").GetComponent<Button>();
+        skipQuestionButton.onClick.Invoke();
+
+        var correctQuestions = GameObject.FindObjectOfType<ScoreKeeper>().GetCorrectAnswers();
+        Assert.AreEqual(1, correctQuestions);
+
+        yield return new WaitForSeconds(3);
+    }
+
+    [UnityTest]
+    public IEnumerator ReplayResetQuestionSeen()
+    {
+        SceneManager.LoadScene("SingleMode");
+        // yield return new WaitForSeconds(3);
+
+        Button replayButton = GameObject.Find("ReplayButton").GetComponent<Button>();
+        replayButton.onClick.Invoke();
+
+        var questionSeen = GameObject.FindObjectOfType<ScoreKeeper>().GetQuestionSeen();
+        Assert.AreEqual(0, questionSeen);
+
+        yield return new WaitForSeconds(3);
+    }
+
+    [UnityTest]
+    public IEnumerator SelectAnswerIncreaseQuestionSeen()
+    {
+        SceneManager.LoadScene("SingleMode");
+        yield return new WaitForSeconds(3);
+
+        var oldQuestionSeen = GameObject.FindObjectOfType<ScoreKeeper>().GetQuestionSeen();
+
+        Button replayButton = GameObject.Find("AnswerButton (0)").GetComponent<Button>();
+        replayButton.onClick.Invoke();
+
+        var newQuestionSeen = GameObject.FindObjectOfType<ScoreKeeper>().GetQuestionSeen();
+        Assert.AreEqual(oldQuestionSeen, newQuestionSeen);
+
+        yield return new WaitForSeconds(3);
+    }
+}
