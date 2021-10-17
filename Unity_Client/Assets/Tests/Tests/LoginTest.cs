cab1fa763a08c08f432938e32dedf3332d3116d1
johnleekj <43510466+johnleekj@users.noreply.github.com>
2021-10-17 12:24:41 +0800

    added invalid input testing for login scene

--- Unity_Client/Assets/Tests/Tests/LoginTest.cs
+++ Unity_Client/Assets/Tests/Tests/LoginTest.cs
@@ -32,4 +32,124 @@ public class LoginTest
 
         yield return new WaitForSeconds(3);
     }
+
+    [UnityTest]
+    public IEnumerator MissingEmail_Input_LoginButton_Onclick_Warning_Text()
+    {
+        SceneManager.LoadScene("LoginFirebase");
+        yield return new WaitForSeconds(3);
+
+        TMP_InputField Email_Input = GameObject.Find("Email_Input").GetComponent<TMP_InputField>();
+        Email_Input.text = "";
+        Email_Input.textComponent.SetText("");
+        TMP_InputField Password_Input = GameObject.Find("Password_Input").GetComponent<TMP_InputField>();
+        Password_Input.text = "123456";
+        Password_Input.textComponent.SetText("123456");
+
+        Button LoginButton = GameObject.Find("Login_Btn").GetComponent<Button>();
+        LoginButton.onClick.Invoke();
+        yield return new WaitForSeconds(1);
+        //TMP_Text Feedback_Text = GameObject.Find("Warning_Text").GetComponent<TMP_Text>();
+        //TextMeshProUGUI feedback_text = GameObject.Find("Confirm_Text").GetComponent<TextMeshProUGUI>();
+        TMP_Text feedback_text = GameObject.Find("Warning_Text").GetComponent<TMP_Text>();
+        Assert.AreEqual("Missing Email", feedback_text.text);
+
+        yield return new WaitForSeconds(3);
+    }
+
+    [UnityTest]
+    public IEnumerator MissingPassword_LoginButton_Onclick_Warning_Text()
+    {
+        SceneManager.LoadScene("LoginFirebase");
+        yield return new WaitForSeconds(3);
+
+        TMP_InputField Email_Input = GameObject.Find("Email_Input").GetComponent<TMP_InputField>();
+        Email_Input.text = "test@test.com";
+        Email_Input.textComponent.SetText("test@test.com");
+        TMP_InputField Password_Input = GameObject.Find("Password_Input").GetComponent<TMP_InputField>();
+        Password_Input.text = "";
+        Password_Input.textComponent.SetText("");
+
+        Button LoginButton = GameObject.Find("Login_Btn").GetComponent<Button>();
+        LoginButton.onClick.Invoke();
+        yield return new WaitForSeconds(1);
+        //TMP_Text Feedback_Text = GameObject.Find("Warning_Text").GetComponent<TMP_Text>();
+        //TextMeshProUGUI feedback_text = GameObject.Find("Confirm_Text").GetComponent<TextMeshProUGUI>();
+        TMP_Text feedback_text = GameObject.Find("Warning_Text").GetComponent<TMP_Text>();
+        Assert.AreEqual("Missing Password", feedback_text.text);
+
+        yield return new WaitForSeconds(3);
+    }
+
+    [UnityTest]
+    public IEnumerator WrongPassword_LoginButton_Onclick_Warning_Text()
+    {
+        SceneManager.LoadScene("LoginFirebase");
+        yield return new WaitForSeconds(3);
+
+        TMP_InputField Email_Input = GameObject.Find("Email_Input").GetComponent<TMP_InputField>();
+        Email_Input.text = "test@test.com";
+        Email_Input.textComponent.SetText("test@test.com");
+        TMP_InputField Password_Input = GameObject.Find("Password_Input").GetComponent<TMP_InputField>();
+        Password_Input.text = "sdfasdf";
+        Password_Input.textComponent.SetText("sdfasdf");
+
+        Button LoginButton = GameObject.Find("Login_Btn").GetComponent<Button>();
+        LoginButton.onClick.Invoke();
+        yield return new WaitForSeconds(1);
+        //TMP_Text Feedback_Text = GameObject.Find("Warning_Text").GetComponent<TMP_Text>();
+        //TextMeshProUGUI feedback_text = GameObject.Find("Confirm_Text").GetComponent<TextMeshProUGUI>();
+        TMP_Text feedback_text = GameObject.Find("Warning_Text").GetComponent<TMP_Text>();
+        Assert.AreEqual("Wrong Password", feedback_text.text);
+
+        yield return new WaitForSeconds(3);
+    }
+
+    [UnityTest]
+    public IEnumerator InvalidEmail_LoginButton_Onclick_Warning_Text()
+    {
+        SceneManager.LoadScene("LoginFirebase");
+        yield return new WaitForSeconds(3);
+
+        TMP_InputField Email_Input = GameObject.Find("Email_Input").GetComponent<TMP_InputField>();
+        Email_Input.text = "safdlkj";
+        Email_Input.textComponent.SetText("safdlkj.com");
+        TMP_InputField Password_Input = GameObject.Find("Password_Input").GetComponent<TMP_InputField>();
+        Password_Input.text = "sdfasdf";
+        Password_Input.textComponent.SetText("sdfasdf");
+
+        Button LoginButton = GameObject.Find("Login_Btn").GetComponent<Button>();
+        LoginButton.onClick.Invoke();
+        yield return new WaitForSeconds(1);
+        //TMP_Text Feedback_Text = GameObject.Find("Warning_Text").GetComponent<TMP_Text>();
+        //TextMeshProUGUI feedback_text = GameObject.Find("Confirm_Text").GetComponent<TextMeshProUGUI>();
+        TMP_Text feedback_text = GameObject.Find("Warning_Text").GetComponent<TMP_Text>();
+        Assert.AreEqual("Invalid Email", feedback_text.text);
+
+        yield return new WaitForSeconds(3);
+    }
+
+    [UnityTest]
+    public IEnumerator UserNotFound_LoginButton_Onclick_Warning_Text()
+    {
+        SceneManager.LoadScene("LoginFirebase");
+        yield return new WaitForSeconds(3);
+
+        TMP_InputField Email_Input = GameObject.Find("Email_Input").GetComponent<TMP_InputField>();
+        Email_Input.text = "notanaccount@test.com";
+        Email_Input.textComponent.SetText("notanaccount@test.com");
+        TMP_InputField Password_Input = GameObject.Find("Password_Input").GetComponent<TMP_InputField>();
+        Password_Input.text = "sdfasdf";
+        Password_Input.textComponent.SetText("sdfasdf");
+
+        Button LoginButton = GameObject.Find("Login_Btn").GetComponent<Button>();
+        LoginButton.onClick.Invoke();
+        yield return new WaitForSeconds(1);
+        //TMP_Text Feedback_Text = GameObject.Find("Warning_Text").GetComponent<TMP_Text>();
+        //TextMeshProUGUI feedback_text = GameObject.Find("Confirm_Text").GetComponent<TextMeshProUGUI>();
+        TMP_Text feedback_text = GameObject.Find("Warning_Text").GetComponent<TMP_Text>();
+        Assert.AreEqual("Account does not exist", feedback_text.text);
+
+        yield return new WaitForSeconds(3);
+    }
 }
