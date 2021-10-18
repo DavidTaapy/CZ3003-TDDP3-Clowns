71917c8981e00fa643b3267a6b2e7432a40f7800
DavidTaapy <DavidTayAP@Hotmail.Com>
2021-10-18 17:46:53 +0800

    Added all scenes to the scene manager

--- Unity_Client/Assets/Scripts/SceneManager.cs
+++ Unity_Client/Assets/Scripts/SceneManager.cs
@@ -6,37 +6,49 @@ using System;
 
 public static class Loader
 {
-    
-    public enum Scene {
+
+    public enum Scene
+    {
         LoadingScene,
         MainMenu,
         LeaderboardScene,
-        RegisterFirebase
+        InventoryScene, ShopScene,
+        RegisterFirebase, LoginFirebase,
+        SingleMode, MultiMode,
+        MultiplayerWinScene, MultiplayerLoseScene,
+        MP_Matched, MP_Finding, ChooseRestaurantScene,
+        ViewSeasonRewardsScene, ReportScene,
+        UserProfileScene, CharacterScene
     }
 
     private static Action onLoaderCallback;
 
-    public static void Load(Scene scene) {
+    public static void Load(Scene scene)
+    {
         // Set the loader callback action to load the target screen
-        onLoaderCallback = () => {
+        onLoaderCallback = () =>
+        {
             SceneManager.LoadScene(scene.ToString());
         };
         // Load the loading screen
         SceneManager.LoadScene(Scene.LoadingScene.ToString());
-    }    
+    }
 
-    public static void LoaderCallback() {
+    public static void LoaderCallback()
+    {
         // Triggered after the first update which lets the screen refresh
         // Execute the loader callback action which will load the target scene
-        if (onLoaderCallback != null) {
+        if (onLoaderCallback != null)
+        {
             // Introducing a 10-cycle delay for loading
             int i = 10;
-            while (i > 0) {
+            while (i > 0)
+            {
                 onLoaderCallback();
                 i--;
             }
             onLoaderCallback = null;
         }
     }
-    
+
 }
