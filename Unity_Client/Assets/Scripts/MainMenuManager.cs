8098655f3a6f43aaf366e308667082bfd1a8daa5
DavidTaapy <DavidTayAP@Hotmail.Com>
2021-10-18 17:47:07 +0800

    Added scene integration for main menu

--- Unity_Client/Assets/Scripts/MainMenuManager.cs
+++ Unity_Client/Assets/Scripts/MainMenuManager.cs
@@ -4,10 +4,35 @@ using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
-    
+
     public void redirectToLeaderboard()
     {
         Loader.Load(Loader.Scene.LeaderboardScene);
+    }
+
+    public void redirectToSingle()
+    {
+        Loader.Load(Loader.Scene.SingleMode);
+    }
+
+    public void redirectToMulti()
+    {
+        Loader.Load(Loader.Scene.MP_Finding);
+    }
+
+    public void redirectToShop()
+    {
+        Loader.Load(Loader.Scene.ShopScene);
+    }
+
+    public void redirectToInventory()
+    {
+        Loader.Load(Loader.Scene.InventoryScene);
+    }
+
+    public void logOut()
+    {
+        Loader.Load(Loader.Scene.LoginFirebase);
     }
 
 }
