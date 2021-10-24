4cf7a2ac7f9fffc8421153a69dec4d59712cbba8
jennifer <gwyjennifer@gmail.com>
2021-10-24 11:59:25 +0800

    Update MultiModeTest

--- Unity_Client/Assets/Tests/Tests/MultiModeTest.cs
+++ Unity_Client/Assets/Tests/Tests/MultiModeTest.cs
@@ -3,23 +3,25 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MultiModeTest
 {
-    // A Test behaves as an ordinary method
-    [Test]
-    public void MultiModeTestSimplePasses()
-    {
-        // Use the Assert class to test conditions
-    }
-
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
     [UnityTest]
-    public IEnumerator MultiModeTestWithEnumeratorPasses()
+    // Test the functionality of updating the user's points after the game ends.
+    public IEnumerator game_end_update_user_points()
     {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
+        SceneManager.LoadScene("MultiMode");
+        yield return new WaitForSeconds(3);
+        
+        var userPoints = GameObject.FindObjectOfType<ScoreKeeper>().CalculatePoints();
+
+        Button skipQuestionButton = GameObject.Find("SkipQuestionButton").GetComponent<Button>();
+        skipQuestionButton.onClick.Invoke();
+        yield return new WaitForSeconds(105);
+
+        var newUserPoints = GameObject.FindObjectOfType<ScoreKeeper>().CalculatePoints();
+        Assert.AreNotEqual(userPoints, newUserPoints, "Failed on the following test: game_end_update_user_points!");
     }
 }
