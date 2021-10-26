89730d554fa49874eb5fd9260fbc815804496676
joy00000 <joy00000@users.noreply.github.com>
2021-10-26 14:44:34 +0800

    get all chars

--- Nodejs_Server/routes/character.routes.js
+++ Nodejs_Server/routes/character.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getCharacter,addCharacter, deleteCharacter } from "../controllers/character.controller.js";
+import { getCharacter,addCharacter, deleteCharacter , getAllCharacters} from "../controllers/character.controller.js";
 
 const router = express.Router();
 
 router.get("/character", getCharacter);
+router.get("/allcharacter", getAllCharacters);
 router.post("/character", addCharacter);
 router.delete("/character", deleteCharacter);
 
