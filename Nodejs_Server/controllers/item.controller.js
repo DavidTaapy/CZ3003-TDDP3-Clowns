92c666f08495253c2a2c8888a026f16b8db5bcb0
ryantanyx <ryantanyx@gmail.com>
2021-10-26 10:10:11 +0800

    done items tests

--- Nodejs_Server/controllers/item.controller.js
+++ Nodejs_Server/controllers/item.controller.js
@@ -8,7 +8,7 @@ const addItems = async(req, res) => {
         itemsdb.add(items).then(function(docRef) {
             itemsdb.doc(docRef.id).set({"id": docRef.id}, { merge: true });
         });
-        res.send("items added");
+        res.send("item added!");
     } catch (error) {
         res.status(400).send(error.message);
         res.send("error adding");
@@ -17,6 +17,13 @@ const addItems = async(req, res) => {
 // Get all items in shop
 const getItems = async(req, res) => {
     try {
+        if (req.query.itemSource == null || req.query.itemSource == ""){
+            res.status(400).send("Missing itemSource param in request!");
+            //return res.send();
+        }
+        if (req.query.itemType == null || req.query.itemType == ""){
+            res.status(400).send("Missing itemType param in request!");
+        }
         const source = req.query.itemSource;
         const type = req.query.itemType;
         var itemsdb = firestore.collection('items');
@@ -31,7 +38,26 @@ const getItems = async(req, res) => {
     }
 };
 
+const deleteItem = async(req, res) => {
+    try {
+        const name = req.query.name;
+        var itemsdb = firestore.collection('items');
+        itemsdb.where('itemName', '==', name).get().then(function(snapshot){
+            if (!snapshot.empty) {
+                snapshot.forEach(function(doc) {
+                    doc.ref.delete();
+                });
+                return res.send("item is deleted!");
+            } else {
+                return res.send("No such item!");
+            }
+        });
+    } catch (error) {
+        return res.status(400).send("invalid name!");
+    }
+};
 
 
 
-export {getItems, addItems};
+
+export {getItems, addItems, deleteItem};
