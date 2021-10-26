f1b42a5bec9805d4210ec943c2974eacf2d1f2c9
ryantanyx <ryantanyx@gmail.com>
2021-10-26 16:56:37 +0800

    added getter and setter for character.cs

--- Unity_Client/Assets/Scripts/Models/Character.cs
+++ Unity_Client/Assets/Scripts/Models/Character.cs
@@ -9,7 +9,7 @@ public class Character
     public string characterID;
     public string characterName;
     public string characterDescription;
-    
+
     public string spriteSource;
 
     Character(){
@@ -18,5 +18,45 @@ public class Character
     public string ToJSON()
     {
         return JsonUtility.ToJson(this);
+    }
+
+    public string getCharacterID()
+    {
+        return this.characterID;
+    }
+
+    public void setCharacterID(string characterID)
+    {
+        this.characterID = characterID;
+    }
+
+    public string getCharacterName()
+    {
+        return this.characterName;
+    }
+
+    public void setCharacterName(string characterName)
+    {
+        this.characterName = characterName;
+    }
+
+    public string getCharacterDescription()
+    {
+        return this.characterDescription;
+    }
+
+    public void setCharacterDescription(string characterDescription)
+    {
+        this.characterDescription = characterDescription;
+    }
+
+    public string getSpriteSource()
+    {
+        return this.spriteSource;
+    }
+
+    public void setSpriteSource(string spriteSource)
+    {
+        this.spriteSource = spriteSource;
     }
 }
