82fdca20e0761ad8dc2735732aab8c8bb6403ca7
johnleekj <43510466+johnleekj@users.noreply.github.com>
2021-11-05 00:22:57 +0800

    Completed select restaurant logic

--- Unity_Client/Assets/Scripts/ChooseRestaurantManager.cs
+++ Unity_Client/Assets/Scripts/ChooseRestaurantManager.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChooseRestaurantManager : MonoBehaviour
 {
     // Start is called before the first frame update
     private int choice = 0;
+    public GameObject CharacterImage;
+    Sprite sprite;
+
     void Start()
     {
-        
+        var linktoUserGet = GameObject.Find("UserDao").GetComponent<UserDao>();
+        Debug.Log("============Starting Data Access Manager========");
+        User user = linktoUserGet.getUser("http://localhost:3000/user", PlayerPrefs.GetString("uid"));
+        Debug.Log(user.getCharacter().getSpriteSource());
+        sprite = Resources.Load<Sprite>(user.getCharacter().getSpriteSource());
+        CharacterImage.GetComponent<Image>().sprite = sprite;
     }
 
     // Update is called once per frame
@@ -53,4 +62,5 @@ public class ChooseRestaurantManager : MonoBehaviour
                 break;
         }
     }
+
 }
